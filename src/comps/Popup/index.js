fc4b049ca9924f34d8657f52020ec258ca81bd3a
Evelyn <evelynzdragon@gmail.com>
2021-01-30 16:50:06 -0800

    comps/Popup 50% in progress

--- src/comps/Popup/index.js
+++ src/comps/Popup/index.js
@@ -1,44 +1,103 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import close from "images/close.png";
 import a1 from "images/a1.png";
 
 const Container = styled.div`
+    min-width: 332px;
+    max-width: 332px;
+    min-height: 423px;
+    max-height: 432px;
+    border: 2px solid #B7B7B7;
+    border-radius: 20px;
+    background-color: rgba(238, 238, 238, 0.9);
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    & > div {
+        margin: 15px 25px;
+        position: relative;
+        display: flex;
+        justify-content: ${props => props.jc ? props.js : "space-between"};
+        & > img {
+            max-width: ${props => props.maxwidth ? props.maxwidth : "67px"};
+            max-height: ${props => props.maxheight ? props.maxheight : "67px"};
+        }
+        & > span {
+            display:flex;
+            flex-direction: column;
+            min-width: 70%;
+            color: ${props => props.color ? props.color : "black"};
+
+            & > span {
+                display:flex;
+                justify-content: space-between;
+                padding: 8px;
+                & > span{
+                    display:flex;
+                }
+            }
+        }
+    }
 
 `;
-const FormRadio = styled.input.attrs({type: 'radio'})`
+const FormRadio = styled.input.attrs({ type: 'radio' })`
 `;
 const Button = styled.button`
-
+    min-width: 122px;
+    max-width: 122px;
+    min-height: 43px;
+    max-height: 43px;
+    background-color: #BD9B52;
+    color: #FFF;
+    border-radius: 6px;
+    border: none;
+    outline: none;
+    &:active{
+        box-shadow: 0 3 px #666;
+        transform: translateY(2px);
+    }
 `;
-const Popup = () =>{
+const Close = styled.img`
+    position: relative;
+    right: -91%;
+`;
+
+
+
+const Popup = ({ onClose }) => {
     return <Container>
         <div>
-            <img src={close} />
+            <Close src={close} onClick={onClose} />
         </div>
         <div>
             <img src={a1} />
-            <span>BC Hydro</span>
             <span>
-                <span>Bill</span>
-                <span>-$49.99</span>
+                <span>BC Hydro</span>
+                <span>
+                    <span style={{color:"#B80000"}}>Bill</span>
+                    <span>-$49.99</span>
+                </span>
             </span>
         </div>
         <div>
-            <span>BC Hydro bill for house</span>
-            <span>To be paid on March 22, 2021</span>
+            <span>
+                <span>BC Hydro bill for house</span>
+                <span>To be paid on March 22, 2021</span>
+            </span>
         </div>
         <div>
-            <FormRadio /> PAID
-            <FormRadio /> PENDING
+            <FormRadio /> <div style={{ color: "#2D7700" }} > PAID</div>
+            <FormRadio /> <div style={{ color: "#FF8A00" }}>PENDING</div>
         </div>
-        <div>
+        <div style={{ justifyContent: "center" }}>
             <Button>OK</Button>
         </div>
 
     </Container>
 }
 Popup.defaultProps = {
-    img: "images/close.svg"
+
 }
 export default Popup;
