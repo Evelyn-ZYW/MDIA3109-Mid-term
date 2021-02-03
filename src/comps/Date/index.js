9c1a024acada23850584a89163ba6321cb054340
juhye7542 <juhye7542@gmail.com>
2021-02-03 03:36:43 -0800

    finished comps

---
+++ src/comps/Date/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const DateCont = styled.div`
+    min-width:375px;
+    max-width:375px;
+    font-size:18px;
+    color:#BD9B52;
+    text-decoration:upercase;
+    font-weight:bold;
+    border-top:2px solid #BD9B52;
+    border-bottom:2px solid #BD9B52;
+    margin:10px;
+
+    div{
+        margin:12px 0px 12px 25px;
+    }
+`;
+
+
+const Date = ({dateText})=>{
+    return <DateCont>
+        <div>{dateText}</div>
+    </DateCont>
+}
+
+Date.defaultProps = {
+dateText:"Thu, jan 07, 2021"
+}
+
+export default Date;
---
+++ src/comps/Header/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const HeaderCont = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    background-color:#C7DEEC;
+    min-width:375px;
+    min-height:65px;
+    position:absolute;
+    top:0px;
+
+    img {
+        max-width:30px;
+        max-height:28px;
+        min-width:15px;
+        max-height:28px;
+    }
+
+    .left {
+        position:relative;
+        right:45px;
+    }
+
+    .right{
+        position:relative;
+        left:12px;
+    }
+
+    div{
+        font-size:24px;
+        min-width:205px;
+        max-width:205px;
+        text-align:center;
+    }
+
+`;
+
+
+const Header = ({headText, iconRight, iconLeft}) =>{
+    return <HeaderCont>
+        <img src={iconLeft} className="left"/><div>{headText}</div><img src={iconRight} className="right"/>
+    </HeaderCont>
+}
+
+Header.defaultProps = {
+    headText:null,
+    iconLeft:null,
+    iconRight:null
+}
+
+export default Header;
